4c6edd63e1859c22c2a12a57337584777ca64281
thankjura <oboz.yuriy@otr.ru>
2016-10-31 20:44:44 +0300

    bump

--- test.c
+++ test.c
@@ -2,7 +2,14 @@
 
 int main() {
     int x;
+get_input:
     x = getchar();
     putchar(x);
     putchar('\n');
+    if (x == '9') {
+        printf("yes!!!");
+        return 0;
+    } else {
+        goto get_input;
+    }
 }
